bf84e618eb7269be219a356b3db43f8639d088ed
Gabrielecorti01 <gcorti.info@gmail.com>
2021-10-28 18:10:18 +0200

    Shop implementato manca utente

--- exercise.php
+++ exercise.php
@@ -1,12 +1,12 @@
 <?php
 
     //classe più generica che comprende tutti gli articoli sportivi dello shop
-    class ShopSport{
+    class Product{
         protected $nome;
         protected $costo;
         protected $descrizione;
 
-        protected function __construct($nome, $costo, $descrizione){
+        public function __construct($nome, $costo, $descrizione){
 
             $this->nome = $nome;
             $this->costo = $costo;
@@ -15,13 +15,15 @@
     }
 
     //classe meno generica che comprende solo le scarpe da calcio
-    class ScarpeCalcio extends ShopSport{
+    class ScarpaCalcio extends Product{
+        use Misura;
         protected $numeroTacchetti;
         protected $colore;
         protected $marca;
 
-        protected function __construct($numeroTacchetti, $colore, $marca, $nome, $costo, $descrizione){
+        public function __construct($numeroTacchetti, $colore, $marca, $taglia, $nome, $costo, $descrizione){
 
+            $this->taglia = $taglia;
             $this->numeroTacchetti = $numeroTacchetti;
             $this->colore = $colore;
             $this->marca = $marca;
@@ -29,7 +31,37 @@
         }
     }
 
+    //classe meno generica che comprende solo le canotte da basket
+    class CanottaBasket extends Product{
+        use Misura;
+        protected $colore;
+        protected $marca;
+
+        public function __construct($colore, $marca, $taglia, $nome, $costo, $descrizione){
+
+            $this->taglia = $taglia;
+            $this->colore = $colore;
+            $this->marca = $marca;
+            parent::__construct($nome, $costo, $descrizione);
+        }
+    }
+
+    trait Misura{
+        public $taglia;
+    }
+
+
     //nuovo oggetto da inserire all'interno della classe Scarpe da Calcio
-    $mercurial = new ScarpeCalcio("11", "giallo", "nike", "mercurial", "270", "ciao sample description ciao",);
+    $mercurial = new ScarpaCalcio(11, "giallo", "nike", "m", "mercurial", 270, "ciao sample description ciao");
+    //nuovo oggetto da inserire all'interno della classe Canotte da basket
+    $jordan = new CanottaBasket("rosso", "nike", "l", "canotta jordan", 100, "ciao seconda descrizione sample ciao" )
     
+    
+
 ?>
+
+<pre>
+    <?php
+        var_dump($mercurial, $jordan);
+    ?>
+</pre>
